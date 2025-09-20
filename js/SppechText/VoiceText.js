0b809641fb889d464dc9278aa2d47ce89e8807b5
Jeeventhiran <131608861+JeeventhiranV@users.noreply.github.com>
2025-09-21 03:42:12 +0530

    Update VoiceText.js

--- js/SppechText/VoiceText.js
+++ js/SppechText/VoiceText.js
@@ -22,20 +22,17 @@
       // Simple parsing
       let type = transcript.includes("income")?"in":"out";
       let amount = Number(transcript.match(/\d+(\.\d+)?/)?.[0]||0);
-      let category = state.dropdowns.categories.find(c => transcript.includes(c.toLowerCase())) || "Misc";
+      let category = state.dropdowns.categories.find(c => transcript.includes(c.toLowerCase())) || "Food";
       let accounts = state.dropdowns.accounts.find(b => transcript.includes(b.toLowerCase())) || "Cash";
-      let ReOccurrences = state.dropdowns.recurrences.find(r => transcript.includes(r.toLowerCase())) || "None";
-      if(ReOccurrences===""){
-        ReOccurrences="None";
-      }
+       
       document.getElementById('fabAddTx').click();  // Open Add Tx Modal
       document.getElementById('tx_type').value = type;
       document.getElementById('tx_amount').value = amount;
       document.getElementById('tx_category').value = category;
       document.getElementById('tx_note').value = transcript;
-      document.getElementById('tx_date').value = nowISO();
+     // document.getElementById('tx_date').value = nowISO();
       document.getElementById('tx_account').value = accounts;
-      document.getElementById('tx_recurrence').value = ReOccurrences;
+     // document.getElementById('tx_recurrence').value = ReOccurrences;
       showToast(`🎤 Detected: ${type} ₹${amount} for ${category}`, 'info');
     });
   }
